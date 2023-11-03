6c6c8a993ebf2afafca5671ca2ba05ba6047be79
nicoIosa <nicolasiosa@hotmail.com>
2023-11-03 14:19:04 -0300

    ejercicio 4 terminado

---
+++ js/ejercicio4.js
@@ -0,0 +1,11 @@
+function indicador(numero) {
+  if (Number.isInteger(numero)) {
+    if (numero % 2 == 0) {
+      document.write("<p>El numero ingresado es PAR</p>");
+    } else document.write("<p>El numero ingresado es IMPAR</p>");
+  } else document.write("No ingresaste un numero valido");
+}
+const numeroIngresado = parseFloat(
+  prompt("Ingrese un numero para determinar si es par o impar")
+);
+indicador(numeroIngresado);
