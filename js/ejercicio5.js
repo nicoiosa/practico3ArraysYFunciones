c0da2122e5da51cbd1b319fe565fc9500dfc39c5
nicoIosa <nicolasiosa@hotmail.com>
2023-11-03 16:57:45 -0300

    ejercicio 5 terminado

---
+++ js/ejercicio5.js
@@ -0,0 +1,13 @@
+function indicador(texto) {
+  if (texto > 0) { document.write("No ingresaste un texto valido")
+  } else {
+    if (texto == texto.toLowerCase()) {
+      document.write("El texto esta formado solo por MINUSCULAS");
+    } else if (texto == texto.toUpperCase()) {
+      document.write("El texto esta formado solo por MAYUSCULAS");
+    } else {
+      document.write("El texto esta formado por MAYUSCULAS y MINUSCULAS");
+    }
+  }
+}
+indicador(prompt("Ingrese una frase"));
